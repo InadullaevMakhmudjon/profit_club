43721d6d0cad812477fc06abfbcac302453766e8
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-22 17:16:40 +0500

    Question preview, end and close endpoints added

--- app/src/main/java/com/example/profitclub/data/urlEndPoints.kt
+++ app/src/main/java/com/example/profitclub/data/urlEndPoints.kt
@@ -17,8 +17,14 @@ const val MAIL_CONFIRM = "user/mail-confirm"
 // User info
 const val USER_INFO = "user/add-info"
 
-// Question consultant view
-const val QUESTIONCONSULTANTVIEW = "main/question-consultant-view"
-
 // Cities, Regions and Categories
-const val GETALLFILTERED = "auto/list"
+const val GET_ALL_FILTERED = "auto/list"
+
+// Question consultant view
+const val QUESTION_CONSULTANT_VIEW = "main/question-consultant-view"
+
+const val QUESTION_CONSULTANT_PREVIEW = "main/question-consultant-preview"
+
+const val QUESTION_CONSULTANT_END = "main/question-consultant-end"
+
+const val QUESTION_CONSULTANT_CLOSE = "main/question-consultant-close"
